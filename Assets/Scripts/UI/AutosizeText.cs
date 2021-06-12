b1de9483fb83c4ef2782370987088ad10aed2dea
Corlio5994 <70762822+Corlio5994@users.noreply.github.com>
2021-06-12 23:26:22 +1000

    The first comments I wrote! More to come starting w the rest of UI

--- Assets/Scripts/UI/AutosizeText.cs
+++ Assets/Scripts/UI/AutosizeText.cs
@@ -1,17 +1,24 @@
 using TMPro;
 using UnityEngine;
-
+//Hey! This is Corey... I'm probably going to write some extra verbose comments for this and the other UI stuff, 
+//as I'm just learning which structures are which in Unity. Feel free to ignore the comments if you're already 
+//familiar with unity modules and stuff! This is more just to help me develop an understanding (:
+//adds text to the autosizetext class using scripting API
 [RequireComponent (typeof (TMP_Text))]
+//all unity classes inherit from MonoBehaviour
 public class AutosizeText : MonoBehaviour {
+    //class has a text and a recttransform attribute
     private TMP_Text text;
     private RectTransform rectTransform;
+    // [SerializeField] is used to tell Unity to show this variable in the editor
     [SerializeField] private float maxWidth = 300;
-
+    //void signifies that the function has no return value 
+    //Gets the text and rectTransform inputs
     void Start () {
         text = GetComponent<TMP_Text> ();
         rectTransform = GetComponent<RectTransform> ();
     }
-
+    //updates by scaling the rectTransform to match the preferred height and width of the text
     void Update () {
         rectTransform.sizeDelta = new Vector2 (Mathf.Min(text.preferredWidth, maxWidth), text.preferredHeight);
     }
